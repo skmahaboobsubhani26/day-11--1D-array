02bf94c062f45b9e1d0a5c1b55e7399adc660373
Shaik Mahaboob subhani <144591124+skmahaboobsubhani26@users.noreply.github.com>
2025-09-05 11:18:54 +0530

    1D array

---
+++ Sum_of_array_elements.java
@@ -0,0 +1,23 @@
+package com.codegnan.arrays;
+import java.util.Scanner;
+public class Sum_of_array_elements {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter array size");
+		int a = sc.nextInt();
+
+		int[] x = new int[a];
+		System.out.println("enter array elements");
+		int sum = 0;
+		for (int i = 0; i < a; i++) {
+			System.out.println("element at index" + i);
+			x[i] = sc.nextInt();
+
+			sum = sum + x[i];
+		}
+		System.out.println(sum);
+
+	}
+
+}
